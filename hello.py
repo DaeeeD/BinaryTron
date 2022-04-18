430003cd7279df8e342a840a3b6045bebc5517ee
DaeeeD <viterbogregoire@gmail.com>
2022-04-18 21:03:47 +0200

    message

--- hello.py
+++ hello.py
@@ -1,13 +1,18 @@
 
 import sys
-import function
-from PyQt5.QtWidgets import (QApplication, QDialog, QLineEdit, QPushButton)
+import binaryTransfo
+from PyQt5.QtWidgets import (QApplication, QDialog, QLineEdit, QPushButton, QVBoxLayout, QWidget)
 
 class Form(QDialog):
 
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
         self.setWindowTitle("Binary Transotron")
+        self.edit = QLineEdit("enter the binary: ")
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.edit)
+
 
 if __name__== '__main__':
     app = QApplication(sys.argv)
