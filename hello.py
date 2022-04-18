e57387167a39f60f5232132dcb339fc48ee71332
DaeeeD <viterbogregoire@gmail.com>
2022-04-18 19:27:52 +0200

    adding buttons

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 
 import sys
+import PyQt5
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QPushButton
 
 app = QApplication(sys.argv)
 
@@ -14,6 +17,11 @@ window.move(60, 15)
 helloMsg = QLabel('<h1> Selecto-Radio! </h1>', parent=window)
 helloMsg.move(60, 15)
 
-window.show()
+layout = QHBoxLayout()
+layout.addWidget(QPushButton('Left'))
+layout.addWidget(QPushButton('Center'))
+layout.addWidget(QPushButton('Right'))
+window.setLayout(layout)
 
+window.show()
 app.exec()
