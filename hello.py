ea8596a5419a5926a81378adcf8dd2dac4d3b099
DaeeeD <viterbogregoire@gmail.com>
2022-05-22 13:01:38 +0200

    adding a second line and fixing some bugs with the input

--- hello.py
+++ hello.py
@@ -29,6 +29,7 @@ if __name__ == '__main__':
     sys.exit(app.exec_()) '''
 
 
+from pickle import TRUE
 import sys
 from typing import Container
 from PyQt5.QtWidgets import QApplication, QWidget,QMainWindow,QPushButton, QLabel, QLineEdit, QVBoxLayout
@@ -37,19 +38,21 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        #fixe le titre et la taille de la fenêtre principale 
         self.setWindowTitle("Da BinaryTron")
+        self.setFixedSize(400,200)
 
         self.label = QLabel()
-
-        self.input = QLineEdit()
-
-        self.button= QPushButton("Let do some magic!")
-
+        self.input = QLineEdit("enter the int here")
+        #ajout d'une seconde ligne pour mettre le resultat en binary 
+        self.input2 = QLineEdit("expecting magic here !")
+        self.button= QPushButton("Let's do some magic!")
         layout = QVBoxLayout()
 
 
         layout.addWidget(self.label)
         layout.addWidget(self.input)
+        layout.addWidget(self.input2)
         layout.addWidget(self.button)
 
         self.button.clicked.connect(self.BinaryTransfo)
@@ -59,10 +62,15 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(container)
 
-#vérifier que la variable binary peut être transformée en int ! sinon message d'erreur 
+    #vérifier que la variable binary peut être transformée en int ! pas en float , ni en str  sinon message d'erreur
     def BinaryTransfo(self):
-        binary = self.input.text()
-        self.input.setText(bin(int(binary)))
+        if self.input.text().isdigit() == True:
+            binary = self.input.text()
+            self.input2.setText(bin(int(binary)))
+        elif self.input.text().isdecimal == True: 
+            self.input2.setText("no float just int !")
+        else:
+            self.input2.setText("Try with a int !")
         
         
 
