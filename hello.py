4df1f825155966259802ac0cb2436cd0d5a898a1
DaeeeD <viterbogregoire@gmail.com>
2022-07-13 11:58:11 +0200

    Final version

--- hello.py
+++ hello.py
@@ -1,33 +1,3 @@
-''' import sys
-
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QDialog
-from PyQt5.QtWidgets import QDialogButtonBox
-from PyQt5.QtWidgets import QFormLayout
-from PyQt5.QtWidgets import QLineEdit
-from PyQt5.QtWidgets import QVBoxLayout
-from PyQt5.QtWidgets import QInputDialog
-
-class Dialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle('Binarytron')
-        dlgLayout = QVBoxLayout()
-        formLayout = QFormLayout()
-        #formLayout.addRow('Numbertransformtron:', QLineEdit())
-        dlgLayout.addLayout(formLayout)
-        btns = QDialogButtonBox()
-        btns.setStandardButtons(
-            QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
-        dlgLayout.addWidget(btns)
-        self.setLayout(dlgLayout)
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    dlg = Dialog()
-    dlg.show()
-    sys.exit(app.exec_()) '''
-
 
 from pickle import TRUE
 import sys
